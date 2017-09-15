511bc926599b5b47179cbc6c1bacb6bdc2cf7038
JacobDomagala <domagala.jacob@gmail.com>
2017-09-15 11:38:51 +0200

    update Sprite class

--- DGame/Sprite.cpp
+++ DGame/Sprite.cpp
@@ -3,10 +3,10 @@
 
 void Sprite::SetSprite(const glm::vec2& position , glm::ivec2 size)
 {
-	glGenVertexArrays(1, &vertexArrayBuffer);
-	glGenBuffers(1, &vertexBuffer);
-	glBindVertexArray(vertexArrayBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glGenVertexArrays(1, &m_vertexArrayBuffer);
+	glGenBuffers(1, &m_vertexBuffer);
+	glBindVertexArray(m_vertexArrayBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
 
 	glm::vec4 positions[6] = {
 		glm::vec4(0.0f + size.x, 0.0f,          1.0f, 1.0f),
@@ -17,15 +17,15 @@ void Sprite::SetSprite(const glm::vec2& position , glm::ivec2 size)
 		glm::vec4(0.0f,          0.0f - size.y, 0.0f, 0.0f),
 		glm::vec4(0.0f + size.x, 0.0f - size.y, 1.0f, 0.0f)
 	};
-	this->centeredPosition.x = position.x + (size.x / 2.0f);
-	this->centeredPosition.y = position.y - (size.y / 2.0f);
-	this->position = position;
-	this->size = size;
+	m_centeredPosition.x = position.x + (size.x / 2.0f);
+	m_centeredPosition.y = position.y - (size.y / 2.0f);
+	m_position = position;
+	m_size = size;
 
-	translateVal = glm::vec3(position, 0.0f);
-	angle = 0.0f;
-	scaleVal = glm::vec2(1.0f, 1.0f);
-	color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	m_translateVal = glm::vec3(position, 0.0f);
+	m_angle = 0.0f;
+	m_scaleVal = glm::vec2(1.0f, 1.0f);
+	m_color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
@@ -35,12 +35,12 @@ void Sprite::SetSprite(const glm::vec2& position , glm::ivec2 size)
 
 std::unique_ptr<byte_vec4> Sprite::SetSpriteTextured(const glm::vec2& position, glm::ivec2 size, const std::string& fileName)
 {
-	std::unique_ptr<byte_vec4> returnPtr = texture.LoadTextureFromFile(fileName);
+	std::unique_ptr<byte_vec4> returnPtr = m_texture.LoadTextureFromFile(fileName);
 
-	glGenVertexArrays(1, &vertexArrayBuffer);
-	glGenBuffers(1, &vertexBuffer);
-	glBindVertexArray(vertexArrayBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glGenVertexArrays(1, &m_vertexArrayBuffer);
+	glGenBuffers(1, &m_vertexBuffer);
+	glBindVertexArray(m_vertexArrayBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
 
 	glm::vec4 positions[6] = {
 		glm::vec4(0.0f + size.x, 0.0f,          1.0f, 1.0f),
@@ -52,39 +52,39 @@ std::unique_ptr<byte_vec4> Sprite::SetSpriteTextured(const glm::vec2& position, 
 		glm::vec4(0.0f + size.x, 0.0f - size.y, 1.0f, 0.0f)
 	};
 
-	this->centeredPosition.x = position.x + (size.x / 2.0f);
-	this->centeredPosition.y = position.y - (size.y / 2.0f);
-	this->position = position;
-	this->size = size;
+	m_centeredPosition.x = position.x + (size.x / 2.0f);
+	m_centeredPosition.y = position.y - (size.y / 2.0f);
+	m_position = position;
+	m_size = size;
 
-	translateVal = glm::vec3(position, 0.0f);
-	angle = 0.0f;
-	scaleVal = glm::vec2(1.0f, 1.0f);
-	color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	m_translateVal = glm::vec3(position, 0.0f);
+	m_angle = 0.0f;
+	m_scaleVal = glm::vec2(1.0f, 1.0f);
+	m_color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
 	glBindVertexArray(0);
 
-	return returnPtr;
+	return std::move(returnPtr);
 }
 
 void Sprite::Render(const Shaders& program)
 {
 	program.UseProgram();
-	glBindVertexArray(vertexArrayBuffer);
+	glBindVertexArray(m_vertexArrayBuffer);
 
 	glm::mat4 modelMatrix;
 
-	modelMatrix = glm::translate(modelMatrix, translateVal);
-	modelMatrix = glm::translate(modelMatrix, glm::vec3((size.x / 2.0f)* scaleVal.x, (size.y / -2.0f) * scaleVal.y, 0.0f));
-	modelMatrix = glm::rotate(modelMatrix, glm::radians(angle), glm::vec3(0.0f, 0.0f, 1.0f));
-	modelMatrix = glm::translate(modelMatrix, glm::vec3((size.x / -2.0f)* scaleVal.x, (size.y / 2.0f) * scaleVal.y, 0.0f));
-	modelMatrix = glm::scale(modelMatrix, glm::vec3(scaleVal, 1.0f));
+	modelMatrix = glm::translate(modelMatrix, m_translateVal);
+	modelMatrix = glm::translate(modelMatrix, glm::vec3((m_size.x / 2.0f)* m_scaleVal.x, (m_size.y / -2.0f) * m_scaleVal.y, 0.0f));
+	modelMatrix = glm::rotate(modelMatrix, glm::radians(m_angle), glm::vec3(0.0f, 0.0f, 1.0f));
+	modelMatrix = glm::translate(modelMatrix, glm::vec3((m_size.x / -2.0f)* m_scaleVal.x, (m_size.y / 2.0f) * m_scaleVal.y, 0.0f));
+	modelMatrix = glm::scale(modelMatrix, glm::vec3(m_scaleVal, 1.0f));
 
-	texture.Use(program.GetProgram());
-	program.SetUniformFloatVec4(color, "color");
+	m_texture.Use(program.GetProgram());
+	program.SetUniformFloatVec4(m_color, "color");
 	program.SetUniformFloatMat4(Win_Window::GetInstance()->GetProjection(), "projectionMatrix");
 	program.SetUniformFloatMat4(modelMatrix, "modelMatrix");
 
--- DGame/Sprite.h
+++ DGame/Sprite.h
@@ -6,47 +6,89 @@
 
 class Sprite 
 {
-	Texture texture;
-	glm::vec4 color;
-	glm::vec2 centeredPosition;
-	glm::vec2 position;
-	GLuint vertexArrayBuffer;
-	GLuint vertexBuffer;
+	// sprite's texture
+	Texture m_texture;
 
-	glm::ivec2 size;
+	// color of sprite (default is white)
+	glm::vec4 m_color;
 
-	glm::vec3 translateVal;
-	glm::vec3 velocity;
-	glm::vec2 scaleVal;
-	float angle;
+	// sprite's center 
+	glm::vec2 m_centeredPosition;
+
+	// sprite's position
+	glm::vec2 m_position;
+
+	// OpenGL buffers
+	GLuint m_vertexArrayBuffer;
+	GLuint m_vertexBuffer;
+
+	// width and height
+	glm::ivec2 m_size;
+
+	// transofmation values
+	glm::vec3 m_translateVal;
+	glm::vec3 m_velocity;
+	glm::vec2 m_scaleVal;
+	float m_angle;
 
 public:
 	Sprite() = default;
 	~Sprite() = default;
 
+	// Create sprite without texture
 	void SetSprite(const glm::vec2& position = glm::vec2(0.0f, 0.0f), glm::ivec2 size = glm::ivec2(10, 10));
-	std::unique_ptr<byte_vec4> SetSpriteTextured(const glm::vec2& position = glm::vec2(0.0f, 0.0f), glm::ivec2 size = glm::ivec2(10, 10), const std::string& fileName = ".\\Default.png");
-	glm::vec2 GetCenteredPosition() const { return centeredPosition; }
-	glm::vec2 GetPosition() const { return position; }
-	glm::ivec2 GetSize() const { return size; }
 
-	void SetColor(const glm::vec3& color) { this->color = glm::vec4(color, 1.0f); }
+	// Create sprite with texture
+	// Returns byte vector used for collision 
+	std::unique_ptr<byte_vec4> SetSpriteTextured(const glm::vec2& position = glm::vec2(0.0f, 0.0f), glm::ivec2 size = glm::ivec2(10, 10), const std::string& fileName = ".\\Default.png");
+	
+	glm::vec2 GetCenteredPosition() const 
+	{ 
+		return m_centeredPosition; 
+	}
+	
+	glm::vec2 GetPosition() const 
+	{ 
+		return m_position; 
+	}
+	
+	glm::ivec2 GetSize() const 
+	{ 
+		return m_size; 
+	}
+
+	void SetColor(const glm::vec3& color) 
+	{ 
+		m_color = glm::vec4(color, 1.0f); 
+	}
 	
 	void SetTextureFromFile(const std::string& filePath)
 	{
-		texture.LoadTextureFromFile(filePath);
+		m_texture.LoadTextureFromFile(filePath);
 	}
+
 	void SetTexture(Texture& texture)
 	{
-		this->texture = texture;
+		m_texture = texture;
 	}
 
-	void Rotate(float angle) { this->angle = angle; }
-	void Scale(const glm::vec2& axies) { scaleVal = axies; }
-	void Translate(const glm::vec2& axies) { 
-		position += axies; 
-		translateVal += glm::vec3(axies, 0.0f); }
+	void Rotate(float angle) 
+	{ 
+		m_angle = angle; 
+	}
 
+	void Scale(const glm::vec2& axies) 
+	{ 
+		m_scaleVal = axies; 
+	}
+
+	void Translate(const glm::vec2& axies) 
+	{ 
+		m_position += axies; 
+		m_translateVal += glm::vec3(axies, 0.0f); 
+	}
+		
+	// Render sprite using 'program' 
 	void Render(const Shaders& program);
 };
 
