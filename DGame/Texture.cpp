d5befa1ea54a806c454351665d2c3df2e9f0eea7
JacobDomagala <domagala.jacob@gmail.com>
2017-09-15 11:24:47 +0200

    update Texture class

--- DGame/Texture.cpp
+++ DGame/Texture.cpp
@@ -2,58 +2,62 @@
 #include "Win_Window.h"
 #include "Shaders.h"
 
-int Texture::unitCounter = 0;
-int Texture::nowBound = 0;
-int Texture::boundCount = 0;
+int Texture::m_unitCounter = 0;
+int Texture::m_nowBound = 0;
+int Texture::m_boundCount = 0;
 
 std::unique_ptr<byte_vec4> Texture::LoadTextureFromFile(const std::string& fileName, GLenum wrapMode, GLenum filter)
 {
-	std::unique_ptr<byte_vec4> returnPtr(reinterpret_cast<byte_vec4*>(SOIL_load_image(fileName.c_str(), &width, &height, NULL, SOIL_LOAD_RGBA)));
+	std::unique_ptr<byte_vec4> returnPtr(reinterpret_cast<byte_vec4*>(SOIL_load_image(fileName.c_str(), &m_width, &m_height, NULL, SOIL_LOAD_RGBA)));
 	
 	if (returnPtr == nullptr)
 	{
 		Win_Window::GetInstance()->ShowError(std::string("Can't load the file ") + fileName, "SOIL error!");
 	}
 
-	glGenTextures(1, &textureID);
-	glBindTexture(GL_TEXTURE_2D, textureID);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, reinterpret_cast<uint8*>(returnPtr.get()));
+	glGenTextures(1, &m_textureID);
+	glBindTexture(GL_TEXTURE_2D, m_textureID);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_width, m_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, reinterpret_cast<uint8*>(returnPtr.get()));
 	glGenerateMipmap(GL_TEXTURE_2D);
 	
-	glSamplerParameteri(samplerID, GL_TEXTURE_MAG_FILTER, filter);
-	glSamplerParameteri(samplerID, GL_TEXTURE_MIN_FILTER, filter);
-	glSamplerParameteri(samplerID, GL_TEXTURE_WRAP_S, wrapMode);
-	glSamplerParameteri(samplerID, GL_TEXTURE_WRAP_T, wrapMode);
+	glSamplerParameteri(m_samplerID, GL_TEXTURE_MAG_FILTER, filter);
+	glSamplerParameteri(m_samplerID, GL_TEXTURE_MIN_FILTER, filter);
+	glSamplerParameteri(m_samplerID, GL_TEXTURE_WRAP_S, wrapMode);
+	glSamplerParameteri(m_samplerID, GL_TEXTURE_WRAP_T, wrapMode);
 
-	unit = unitCounter++;
+	m_unit = m_unitCounter++;
 
 	return std::move(returnPtr);
 }
 
 void Texture::LoadTextureFromMemory(int width, int height, uint8* data, GLenum wrapMode, GLenum filter)
 {
-	glGenTextures(1, &textureID);
-	glBindTexture(GL_TEXTURE_2D, textureID);
+	glGenTextures(1, &m_textureID);
+	glBindTexture(GL_TEXTURE_2D, m_textureID);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 	glGenerateMipmap(GL_TEXTURE_2D);
 
-	glSamplerParameteri(samplerID, GL_TEXTURE_MAG_FILTER, filter);
-	glSamplerParameteri(samplerID, GL_TEXTURE_MIN_FILTER, filter);
-	glSamplerParameteri(samplerID, GL_TEXTURE_WRAP_S, wrapMode);
-	glSamplerParameteri(samplerID, GL_TEXTURE_WRAP_T, wrapMode);
-	unit = unitCounter++;
+	glSamplerParameteri(m_samplerID, GL_TEXTURE_MAG_FILTER, filter);
+	glSamplerParameteri(m_samplerID, GL_TEXTURE_MIN_FILTER, filter);
+	glSamplerParameteri(m_samplerID, GL_TEXTURE_WRAP_S, wrapMode);
+	glSamplerParameteri(m_samplerID, GL_TEXTURE_WRAP_T, wrapMode);
+
+	m_unit = m_unitCounter++;
 }
 
 void Texture::Use(GLuint program)
 {
-	if (nowBound != unit)
+	if (m_nowBound != m_unit)
 	{
 		GLuint samplerLocation = glGetUniformLocation(program, "texture");
-		glActiveTexture(GL_TEXTURE0 + unit);
-		glBindTexture(GL_TEXTURE_2D, textureID);
-		glUniform1i(samplerLocation, unit);
-		nowBound = unit;
-		++boundCount;
+
+		glActiveTexture(GL_TEXTURE0 + m_unit);
+		glBindTexture(GL_TEXTURE_2D, m_textureID);
+		glUniform1i(samplerLocation, m_unit);
+
+		m_nowBound = m_unit;
+
+		++m_boundCount;
 	}
 }
 
--- DGame/Texture.h
+++ DGame/Texture.h
@@ -4,28 +4,56 @@
 
 class Texture 
 {
-	int width, height;
-	GLuint textureID;
-	GLuint samplerID;
-	uint8* fileData;
-	static int unitCounter;
-	static int nowBound;
-	int unit;
+	// width and size of texture
+	int m_width;
+	int m_height;
+
+	// texture ID used in
+	GLuint m_textureID;
+
+	// sampler ID
+	GLuint m_samplerID;
+
+	// each time new texture is loaded this counter is increased
+	static int m_unitCounter;
+
+	// ID of currently bound texture
+	static int m_nowBound;
+
+	// texture unit
+	int m_unit;
 
 public:
 	Texture() = default;
 	~Texture() = default;
 	
-	static int boundCount;
+	// DEBUG: number of glBindTexture calls
+	static int m_boundCount;
 
+	// Load texture from 'fileName' file and return byte values (used for collision)
 	std::unique_ptr<byte_vec4> LoadTextureFromFile(const std::string& fileName = "Assets//Default.png", 
 							 GLenum wrapMode = GL_REPEAT, GLenum filter = GL_LINEAR);
+
+	// Load texture from 'data' memory
 	void LoadTextureFromMemory(int width, int height, uint8* data, 
 							   GLenum wrapMode = GL_REPEAT, GLenum filter = GL_LINEAR);
-	int GetWidth() const { return width; }
-	int GetHeight() const { return height; }
-	GLuint GetTextureHandle() const { return textureID; }
 
+	int GetWidth() const 
+	{ 
+		return m_width; 
+	}
+
+	int GetHeight() const 
+	{ 
+		return m_height; 
+	}
+	
+	GLuint GetTextureHandle() const 
+	{ 
+		return m_textureID; 
+	}
+
+	// Make this texture active for given 'programID'
 	void Use(GLuint programID);
 };
 
