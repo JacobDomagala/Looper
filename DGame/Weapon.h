9e0de1265e35759bc791ea11d0fa7fddf0e2277f
JacobDomagala <domagala.jacob@gmail.com>
2017-09-15 09:30:17 +0200

    Update Weapon file

--- DGame/Weapon.h
+++ DGame/Weapon.h
@@ -5,52 +5,48 @@
 class Weapon 
 {
 protected:
-	int range;
-	int dmg;
-	int ammoNum;
-	bool hasAmmo;
-	std::string name;
-	float reloadTime;
+	int m_range;
+	int m_dmg;
+	int m_ammoNum;
+	bool m_hasAmmo;
+	std::string m_name;
+	float m_reloadTime;
+
+	Weapon(int range, int dmg, int ammoNum, bool hasAmmo, std::string name, float reloadTime):
+		m_range(range),
+		m_dmg(dmg),
+		m_ammoNum(ammoNum),
+		m_hasAmmo(hasAmmo),
+		m_name(name),
+		m_reloadTime(reloadTime)
+	{}
 
 public:
-	Weapon() = default;
 	virtual ~Weapon() = default;
 	
-	virtual std::string GetName() const { return name; }
-	virtual int GetAmmoNum() const { return ammoNum; }
-	virtual float GetReloadTime() const { return reloadTime; }
-	virtual int GetRange() const { return range; }
-	virtual int GetDamage() const { return dmg; }
-	virtual void Shoot(const glm::vec2& direction) {}
+	virtual std::string GetName() const { return m_name; }
+	virtual int GetAmmoNum() const { return m_ammoNum; }
+	virtual float GetReloadTime() const { return m_reloadTime; }
+	virtual int GetRange() const { return m_range; }
+	virtual int GetDamage() const { return m_dmg; }
+	virtual void Shoot(const glm::vec2& direction) = 0;
 };
 
 class SniperRifle : public Weapon 
 {
 
 public:
-	SniperRifle()
-	{
-		range = 3000;
-		dmg = 80;
-		name = "sniper rifle";
-		hasAmmo = true;
-		ammoNum = 10;
-		reloadTime = 1.0f;
-	}
+	SniperRifle():
+		Weapon(3000, 80, 10, true, { "sniper rifle" }, 1.0f)
+	{}
 	virtual ~SniperRifle() = default;
 };
 
 class Glock : public Weapon 
 {
 public:
-	Glock()
-	{
-		range = 3000;
-		dmg = 10;
-		name = "Glock";
-		hasAmmo = true;
-		ammoNum = 20;
-		reloadTime = 0.3f;
-	}
+	Glock():
+		Weapon(3000, 10, 20, true, { "glock" }, 0.3f)
+	{}
 	virtual ~Glock() = default;
 };
