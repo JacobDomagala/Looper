9a339d1e2e264063d162c536864b8ca1cb9a9165
Jakub Domagala <domagala.jacob@gmail.com>
2021-02-17 22:56:10 +0100

    [#30]: Disable cpp-check from checking dependencies

--- cmake/static_analysis.cmake
+++ cmake/static_analysis.cmake
@@ -12,7 +12,7 @@ if(ENABLE_CPPCHECK)
         --inline-suppr
         --inconclusive
         -i
-        ${CMAKE_SOURCE_DIR}/imgui/lib)
+        ${DEPENDENCIES_PATH})
   else()
     message(SEND_ERROR "cppcheck requested but executable not found")
   endif()
